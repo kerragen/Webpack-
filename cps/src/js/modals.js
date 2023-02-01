8f0cea398e60f41475b451c1392997cf1b4b610f
Alexander Bazhenov <cfifxxx909@gmail.com>
2023-02-02 04:45:20 +0500

    1.6 fix modals & scroll

--- cps/src/js/modals.js
+++ cps/src/js/modals.js
@@ -1,3 +1,5 @@
+const body = document.body
+
 // Переменные бокового меню //
 const leftMenu = document.querySelector('.left-menu')
 const menuButton = document.querySelector('.burger')
@@ -10,39 +12,44 @@ const callButtons = document.querySelectorAll('.call')
 const modalCall = document.querySelector('.modal-call')
 const modalClose = document.querySelector('.call-close')
 const callFog = document.querySelector('.modal-call__fog')
+const callSubmit = document.querySelector('.modal-call__send')
 
 // Переменные фидбэка //
 const feedbackButtons = document.querySelectorAll('.feedback')
 const modalFeedback = document.querySelector('.modal-fb')
 const feedbackClose = document.querySelector('.fb-close')
 const feedbackFog = document.querySelector('.modal-fb__fog')
-
+const feedbackSubmit = document.querySelector('.modal-fb__send')
 
 
 // Функционал звонка //
 for (let callButton of callButtons) {
   callButton.addEventListener('click', function () {
     modalCall.classList.toggle('modal-call--open')
-    document.body.style.overflow = 'hidden'
+    body.classList.add('opened')
   })
 }
 
 modalClose.addEventListener('click', function () {
   modalCall.classList.toggle('modal-call--open')
-  if (leftMenu.classList.contains('left-menu--open')) {
-    document.body.style.overflow = 'hidden'
+  if(leftMenu.classList.contains('left-menu--open')){
+    body.classList.add('opened')
   } else {
-    document.body.style.overflow = ''
+    body.classList.remove('opened')
   }
 })
 
 callFog.addEventListener('click', function () {
   modalCall.classList.toggle('modal-call--open')
-  if (leftMenu.classList.contains('left-menu--open')) {
-    document.body.style.overflow = 'hidden'
+  if(leftMenu.classList.contains('left-menu--open')){
+    body.classList.add('opened')
   } else {
-    document.body.style.overflow = ''
+    body.classList.remove('opened')
   }
+})
+
+callSubmit.addEventListener('click', function(e) {
+  e.preventDefault()
 })
 
 
@@ -50,41 +57,49 @@ callFog.addEventListener('click', function () {
 for (let feedbackButton of feedbackButtons) {
   feedbackButton.addEventListener('click', function () {
     modalFeedback.classList.toggle('modal-fb--open')
-    document.body.style.overflow = 'hidden'
+    body.classList.add('opened')
   })
 }
 
 feedbackClose.addEventListener('click', function () {
   modalFeedback.classList.toggle('modal-fb--open')
-  if (leftMenu.classList.contains('left-menu--open')) {
-    document.body.style.overflow = 'hidden'
+  if(leftMenu.classList.contains('left-menu--open')){
+    body.classList.add('opened')
   } else {
-    document.body.style.overflow = ''
+    body.classList.remove('opened')
   }
 })
 
+
+
 feedbackFog.addEventListener('click', function () {
   modalFeedback.classList.toggle('modal-fb--open')
-  if (leftMenu.classList.contains('left-menu--open')) {
-    document.body.style.overflow = 'hidden'
+  if(leftMenu.classList.contains('left-menu--open')){
+    body.classList.add('opened')
   } else {
-    document.body.style.overflow = ''
+    body.classList.remove('opened')
   }
 })
+
+feedbackSubmit.addEventListener('click', function(e){
+  e.preventDefault()
+})
+
 
 
 // Функционал бокового меню //
 menuButton.addEventListener('click', function () {
   leftMenu.classList.toggle('left-menu--open')
-  document.body.style.overflow = 'hidden'
+  body.classList.add('opened')
 })
 
 menuClose.addEventListener('click', function () {
   leftMenu.classList.toggle('left-menu--open')
-  document.body.style.overflow = ''
+  body.classList.remove('opened')
 })
 
 menuFog.addEventListener('click', function () {
   leftMenu.classList.toggle('left-menu--open')
-  document.body.style.overflow = ''
+  body.classList.remove('opened')
 })
+
